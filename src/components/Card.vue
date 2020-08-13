d825d94f162e4af45e3072ef97c51ef176a63ac0
Milan Kracht <milan@domini.nl>
2020-08-13 09:18:05 +0200

    Initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    Message: {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Card extends Vue {
+  @Prop() public msg?: string;
+}
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    padding: 16px;
+    background: white;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(black, 0.15);
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Card msg="This is a card" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
+
+@Component({
+  components: {
+    HelloWorld,
+    Card
+  }
+})
+export default class Home extends Vue {}
+</script>
